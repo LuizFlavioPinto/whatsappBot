f06dc512c34fd0ad6737bd45c979cf087537e946
Luiz Flavio <luizflavio1452@gmail.com>
2021-04-09 21:02:44 -0300

    Modo repeater adicionado e aprimoramentos no código

--- index.js
+++ index.js
@@ -1,5 +1,11 @@
 const puppeteer = require("puppeteer")
 
+function delay(time) {
+  return new Promise(function(resolve) { 
+      setTimeout(resolve, time)
+  });
+}
+
 let sendMessage = async (contactName, typeOfMessage, amountOfMessages, message) => {
   const browser = await puppeteer.launch({ headless: false });
   const page = await browser.newPage();
@@ -11,13 +17,24 @@ let sendMessage = async (contactName, typeOfMessage, amountOfMessages, message) 
   const editor = await page.$("div[data-tab='6']");
   await editor.focus();
 
+  let element
+
   const functions = {
     normalMessage () {
       message = message
     },
-    repeater () {
-      let messages = document.querySelectorAll('span._3-8er.selectable-text.copyable-text > span')
-      message = messages[messages.length - 1].innerText
+    async repeater () {
+      for(let i = false; i == false; i = i){
+        message = element
+        element = await page.$$eval('span._3-8er.selectable-text.copyable-text > span', 
+        el => el[el.length - 1].innerText)
+
+        if (element != message && element != '') message = element, i = true
+
+        // element != '' used because when the message is an emoji, the bot gets an error
+
+        await delay(1500)
+      }
     },
     randomFlood() {
       let characters = ['a','b','c','d','e','f','g','h','i','j','k','l','m','n','o','p',
@@ -41,10 +58,12 @@ let sendMessage = async (contactName, typeOfMessage, amountOfMessages, message) 
   for (let i = 0; i < amountOfMessages; i++) {
     if(typeOfMessage == 'normalMessage')functions.normalMessage()
     if(typeOfMessage == 'randomFlood')functions.randomFlood()
+    if(typeOfMessage == 'repeater')await functions.repeater()
 
     await page.keyboard.sendCharacter(message);
     await page.click("span[data-testid='send']");
   }
+  await console.log('Finished')
 }
 
-sendMessage('Fernando', 'normalMessage', 1000, 'Hello')
+sendMessage('Fernando', 'normalMessage', 10, 'FOGÃO SEMPRE PERTO DO TÍTULO')
