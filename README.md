1c946f4d6506526a646b7207a30488667f74e2ff
Luiz Flavio <luizflavio1452@gmail.com>
2021-04-26 13:27:46 -0300

    config.json adicionado, melhorias no código e readme atualizado

--- README.md
+++ README.md
@@ -1,6 +1,6 @@
 # Whatsapp-Bot
 
-- This is a Bot that sends messages automatically to another person in Whatsapp.
+- This is a Bot that sends messages automatically to another person or group in Whatsapp.
 
 ## Technologies:
 - This was made in NodeJS with the Puppeteer library.
@@ -8,18 +8,20 @@
 ## How to use it:
 - After cloning the project, go to the terminal and write: npm install puppeteer; to install the puppeteer library
 
-- Then, change the params of the sendMessage function. These are the params that you have to put: 
+- Then, change the params on config.json. These are the params that you have to put: 
 
-    1° = Contact name. The contact name must be at least on the five last contacts that you talked with
+    1° = Contact name. The contact name must be at least on the six last contacts that you talked with
 
     2° = Type of message. The possibilities are:
 
-        * normalMessage, message defined at the fourth param;
+        * Normal Message, message defined at the fourth param;
 
-        * randomFlood, message with random characters
+        * Random Flood, message with random characters
 
-        * repeater, under development ...
+        * Repeater, takes de last sent message of the person and sends it again 
 
     3° = Amount of messages that you want to send
 
-    4° = The Message(not required when using the randomFlood mode)
+    4° = The Message(just required when using the Normal Message mode)
+
+- Then, execute the program and have fun pissing of your friends 
--- index.js
+++ index.js
@@ -1,4 +1,10 @@
-const puppeteer = require("puppeteer")
+const puppeteer = require("puppeteer"),
+      contactName = require("./config.json").ContactName,
+      typeOfMessage = require('./config.json').TypeOfMessage,
+      amountOfMessages = require('./config.json').AmountOfMessages,
+      message = require('./config.json').Message;
+      
+      
 
 function delay(time) {
   return new Promise(function(resolve) { 
@@ -6,7 +12,7 @@ function delay(time) {
   });
 }
 
-let sendMessage = async (contactName, typeOfMessage, amountOfMessages, message) => {
+(async (contactName, typeOfMessage, amountOfMessages, message) => {
   const browser = await puppeteer.launch({ headless: false });
   const page = await browser.newPage();
   await page.goto("https://web.whatsapp.com/");
@@ -25,14 +31,14 @@ let sendMessage = async (contactName, typeOfMessage, amountOfMessages, message) 
     },
     async repeater () {
       for(let i = false; i == false; i = i){
-        message = element
         element = await page.$$eval('span._3-8er.selectable-text.copyable-text > span', 
         el => el[el.length - 1].innerText)
 
         if (element != message && element != '') message = element, i = true
 
-        // element != '' used because when the message is an emoji, the bot gets an error
-
+        /*  element != '' used because when the message is an emoji, the el.innerText
+            returns a ''
+        */
         await delay(1500)
       }
     },
@@ -40,7 +46,7 @@ let sendMessage = async (contactName, typeOfMessage, amountOfMessages, message) 
       let characters = ['a','b','c','d','e','f','g','h','i','j','k','l','m','n','o','p',
       'q','r','s','t','u','v','w','x','y','z','ç','!','@','#','$','%','¨','&','*','(',')',
       '-','_','+','=','`','´','[',']','{','}','^','~',',','<','.','>',':',';','/','?','°',
-      '¹','²','³','£','¢','¬'];
+      '¹','²','³','£','¢','¬','|'];
     
       function randomWord() {
         let generatedNumber = Math.floor(Math.random() * ((characters.length - 1) - 0) + 0)
@@ -54,16 +60,20 @@ let sendMessage = async (contactName, typeOfMessage, amountOfMessages, message) 
       }  
     }
   }
+
+  function treatTypeOfMessageVariable () {
+    typeOfMessage = typeOfMessage.toLowerCase().replace(/\s/g, '')
+  }
+
+  treatTypeOfMessageVariable()
   
   for (let i = 0; i < amountOfMessages; i++) {
-    if(typeOfMessage == 'normalMessage')functions.normalMessage()
-    if(typeOfMessage == 'randomFlood')functions.randomFlood()
-    if(typeOfMessage == 'repeater')await functions.repeater()
+    if(typeOfMessage === 'normalmessage')functions.normalMessage()
+    if(typeOfMessage ==='randomflood')functions.randomFlood()
+    if(typeOfMessage === 'repeater')await functions.repeater()
 
     await page.keyboard.sendCharacter(message);
     await page.click("span[data-testid='send']");
   }
   await console.log('Finished')
-}
-
-sendMessage('Fernando', 'normalMessage', 10, 'FOGÃO SEMPRE PERTO DO TÍTULO')
+})(contactName, typeOfMessage, amountOfMessages, message)
